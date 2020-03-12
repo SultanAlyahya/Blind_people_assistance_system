7526590ff9dfec01e3fdc7affc23a3742755d717
Saad <s.alajlan10@gmail.com>
2020-03-12 07:08:51 +0300

    Fix Camera

--- views/blindUI/currency.js
+++ views/blindUI/currency.js
@@ -12,151 +12,147 @@ import { fetch } from '@tensorflow/tfjs-react-native'
 import * as ImagePicker from 'expo-image-picker'
 
 export default class currency extends React.Component{
-  constructor(props) {
-  super(props);
-  this.state = {
+  state = {
     isTfReady: false,
     isModelReady: false,
     predictions: null,
-    image: null
-  };
-}
- 
-selectImage = async () => {
-  try {
-    let response = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3]
+    image: {uri:this.props.navigation.state.params.imagePath} ,
+    imageURI:this.props.navigation.state.params.imagePath 
+  }
+
+  async componentDidMount() {
+    await tf.ready()
+    this.setState({
+      isTfReady: true
     })
+    this.model = await mobilenet.load()
+    this.setState({ isModelReady: true })
+    this.getPermissionAsync()
+  }
 
-    if (!response.cancelled) {
-      const source = { uri: response.uri }
-      this.setState({ image: source })
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!')
+      }
+    }
+  }
+
+  imageToTensor(rawImageData) {
+    const TO_UINT8ARRAY = true
+    const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
+    // Drop the alpha channel info for mobilenet
+    const buffer = new Uint8Array(width * height * 3)
+    let offset = 0 // offset into original data
+    for (let i = 0; i < buffer.length; i += 3) {
+      buffer[i] = data[offset]
+      buffer[i + 1] = data[offset + 1]
+      buffer[i + 2] = data[offset + 2]
+
+      offset += 4
+    }
+
+    return tf.tensor3d(buffer, [height, width, 3])
+  }
+
+  classifyImage = async () => {
+    try {
+      const imageAssetPath = Image.resolveAssetSource(this.state.image)
+      const response = await fetch(imageAssetPath.uri, {}, { isBinary: true })
+      const rawImageData = await response.arrayBuffer()
+      const imageTensor = this.imageToTensor(rawImageData)
+      const predictions = await this.model.classify(imageTensor)
+      this.setState({ predictions })
+      console.log(predictions)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  selectImage = async () => {
+    try {
+      // let response = await ImagePicker.launchImageLibraryAsync({
+      //   mediaTypes: ImagePicker.MediaTypeOptions.All,
+      //   allowsEditing: true,
+      //   aspect: [4, 3]
+      // })
+
+      // if (!response.cancelled) {
+      //   const source = { uri: response.uri }
+      //   console.log(source)
+      //   this.setState({ image: source })
+      //   this.classifyImage()
+      // }
+      // console.log(this.state.imageURI)
+      // const source = { uri: this.state.imageURI }
+      // console.log(source)
+      // this.setState({ image: source })
       this.classifyImage()
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
-classifyImage = async () => {
-  try {
-    const imageAssetPath = Image.resolveAssetSource(this.state.image)
-    const response = await fetch(imageAssetPath.uri, {}, { isBinary: true })
-    const rawImageData = await response.arrayBuffer()
-    const imageTensor = this.imageToTensor(rawImageData)
-    const predictions = await this.model.classify(imageTensor)
-    this.setState({ predictions })
-    console.log(predictions)
-  } catch (error) {
-    console.log(error)
-  }
-}
-imageToTensor(rawImageData) {
-  const TO_UINT8ARRAY = true
-  const { width, height, data } = jpeg.decode(rawImageData, TO_UINT8ARRAY)
-  // Drop the alpha channel info for mobilenet
-  const buffer = new Uint8Array(width * height * 3)
-  let offset = 0 // offset into original data
-  for (let i = 0; i < buffer.length; i += 3) {
-    buffer[i] = data[offset]
-    buffer[i + 1] = data[offset + 1]
-    buffer[i + 2] = data[offset + 2]
-
-    offset += 4
-  }
-
-
-  const takePicture=async()=>{
-    try{
-    //this method is for taking pictures
-    const image= await this.camera.takePictureAsync({base64:true})
-    //the return value is a uri and the image is saved in the mobile cash
-    //you can access it from the <Image> </Image> by providing the returned uri, width and height
-    navigation.navigate('imageP', {imagePath:image.uri, base64:image.base64,image:image})
-    }catch(error){
-      console.log(error.message)
-  return tf.tensor3d(buffer, [height, width, 3])
-}
-getPermissionAsync = async () => {
-  if (Constants.platform.ios) {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
-    if (status !== 'granted') {
-      alert('Sorry, we need camera roll permissions to make this work!')
+    } catch (error) {
+      console.log(error)
     }
   }
-}
 
-async componentDidMount() {
-  // Wait for tf to be ready.
-  await tf.ready();
-  // Signal to the app that tensorflow.js can now be used.
-  this.setState({
-    isTfReady: true,
-  })
-  this.model = await mobilenet.load()
-  this.setState({ isModelReady: true })
-  this.getPermissionAsync()
-}
+  renderPrediction = prediction => {
+    return (
+      <Text key={prediction.className} style={styles.text}>
+        {prediction.className}
+      </Text>
+    )
+  }
 
-renderPrediction = prediction => {
-  return (
-    <Text key={prediction.className} style={styles.text}>
-      {prediction.className}
-    </Text>
-  )
-}
+  render() {
+    const { isTfReady, isModelReady, predictions, image, imageURI } = this.state
 
-render() {
-  const { isTfReady, isModelReady, predictions, image } = this.state
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle='light-content' />
+        <View style={styles.loadingContainer}>
+          <Text style={styles.text}>
+            TFJS ready? {isTfReady ? <Text>✅</Text> : ''}
+          </Text>
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle='light-content' />
-      <View style={styles.loadingContainer}>
-        <Text style={styles.commonTextStyles}>
-          TFJS ready? {isTfReady ? <Text>✅</Text> : ''}
-        </Text>
+          <View style={styles.loadingModelContainer}>
+            <Text style={styles.text}>Model ready? </Text>
+            {isModelReady ? (
+              <Text style={styles.text}>✅</Text>
+            ) : (
+              <ActivityIndicator size='small' />
+            )}
+            
+          </View>
+          
+        </View>
+     
+        <TouchableOpacity
+          style={styles.imageWrapper}
+          onPress={isModelReady ? this.selectImage : undefined}>
+          {image && <Image source={image} style={styles.imageContainer} />}
 
-        <View style={styles.loadingModelContainer}>
-          <Text style={styles.text}>Model ready? </Text>
-          {isModelReady ? (
-            <Text style={styles.text}>✅</Text>
-          ) : (
-            <ActivityIndicator size='small' style={{color: 'black'}} />
+          {isModelReady && !image && (
+            <Text style={styles.transparentText}>Tap to choose image</Text>
           )}
+        </TouchableOpacity>
+        
+        <View style={styles.predictionWrapper}>
+       
+          {isModelReady && image && (
+            <Text style={styles.text}>
+              Predictions: {predictions ? '' : 'Predicting...'}
+            </Text>
+          )}
+          {isModelReady &&
+            predictions &&
+            predictions.map(p => this.renderPrediction(p))}
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.poweredBy}>Powered by:</Text>
+          
         </View>
       </View>
-      <TouchableOpacity
-        style={styles.imageWrapper}
-        onPress={isModelReady ? this.selectImage : undefined}>
-        {image && <Image source={image} style={styles.imageContainer} />}
-
-        {isModelReady && !image && (
-          <Text style={styles.transparentText}>اضغط لإختيار صورة</Text>
-        )}
-      </TouchableOpacity>
-      <View style={styles.predictionWrapper}>
-        {isModelReady && image && (
-          <Text style={styles.text}>
-            Predictions: {predictions ? '' : 'Predicting...'}
-          </Text>
-        )}
-        {isModelReady &&
-          predictions &&
-          predictions.map(p => this.renderPrediction(p))}
-      </View>
-      <TouchableOpacity style={styles.flipButton}
-                onPress={()=> this.props.navigation.navigate('CameraP')}>
-                    <Text style={styles.flipText}>التقاط صورة</Text>
-                </TouchableOpacity>
-      <View style={styles.footer}>
-        
-       
-      </View>
-    </View>
-  )
-}
+    )
+  }
 }
 const styles = StyleSheet.create({
 container: {
